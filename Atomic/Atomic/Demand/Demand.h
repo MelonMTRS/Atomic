f32caf38043d6d66e314abf97405ef9972420892
viown <48097677+viown@users.noreply.github.com>
2020-10-24 12:09:41 +0300

    getAllDemand()

--- Atomic/Atomic/Demand/Demand.h
+++ Atomic/Atomic/Demand/Demand.h
@@ -1,8 +1,11 @@
 #ifndef __ATOMIC__DEMAND__DEMAND__H
 #define __ATOMIC__DEMAND__DEMAND__H
+#include <vector>
+#include "../API/Rolimons.h"
 #include "../Item.h"
 
 namespace atomic {
+	std::vector<atomic::Item> getAllDemand(rolimons::ItemDB& items); // Asynchronously gets the demand of all items with an unassigned tag
 	atomic::Demand getDemand(int assetId);
 }
 
