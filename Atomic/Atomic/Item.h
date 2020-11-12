9c2925055f2b6920bb789de27082794ccff1b183
viown <48097677+viown@users.noreply.github.com>
2020-11-12 11:02:43 +0300

    Removed atomic::Demand::NotSet

--- Atomic/Atomic/Item.h
+++ Atomic/Atomic/Item.h
@@ -3,12 +3,7 @@
 #include <string>
 
 namespace atomic {
-	/*
-	some kids may ask why there is both NotSet and NotAssigned
-	and to those kids i say ur a dumb idiot cum poop
-	*/
 	enum class Demand {
-		NotSet,
 		NotAssigned,
 		Terrible,
 		Low,
@@ -23,7 +18,7 @@ namespace atomic {
 		std::int64_t id;
 		std::int64_t rap;
 		std::int64_t value = -1;
-		atomic::Demand demand = atomic::Demand::NotSet;
+		atomic::Demand demand = atomic::Demand::NotAssigned;
 
 		void operator=(const Item* newItem) {
 			*this = newItem;
@@ -34,7 +29,7 @@ namespace atomic {
 		std::int64_t userAssetId;
 		UniqueItem() = default;
 		UniqueItem(std::string p_name, std::int64_t p_id, std::int64_t p_userAssetId,
-		   std::int64_t p_rap, std::int64_t p_value=-1, atomic::Demand p_demand = Demand::NotSet)
+		   std::int64_t p_rap, std::int64_t p_value=-1, atomic::Demand p_demand = Demand::NotAssigned)
 			: Item{ p_name, p_id, p_rap, p_value, p_demand }, userAssetId{p_userAssetId} {}
 
 		Item getRegularItem() {
