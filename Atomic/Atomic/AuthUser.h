0e7dab48f35701598860036b7a4c533048eae4fb
viown <48097677+viown@users.noreply.github.com>
2020-11-13 10:36:29 +0300

    Passing name to getUser()

--- Atomic/Atomic/AuthUser.h
+++ Atomic/Atomic/AuthUser.h
@@ -20,7 +20,7 @@ namespace atomic {
 		}
 
 		inline void setCookie(const std::string& newCookie) noexcept { this->m_cookie = newCookie; }
-		[[nodiscard]] inline const atomic::User getUser() const noexcept { return atomic::User{ this->m_id }; };
+		[[nodiscard]] inline const atomic::User getUser() const noexcept { return atomic::User{ this->m_id, this->m_name }; };
 		[[nodiscard]] inline const std::string name() const noexcept { return this->m_name; }
 		[[nodiscard]] inline const std::string getCookie() const noexcept { return this->m_cookie; }
 		[[nodiscard]] inline const std::string getXcsrf() const noexcept { return this->m_xcsrf; }
