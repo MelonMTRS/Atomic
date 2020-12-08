1e4c8cc9dde573a88362a84c0c315160704d17f5
viown <48097677+viown@users.noreply.github.com>
2020-12-08 11:20:25 +0000

    Updated link to default config

--- Atomic/Atomic/Config/Config.cpp
+++ Atomic/Atomic/Config/Config.cpp
@@ -15,8 +15,7 @@ bool config::configExists() {
 }
 
 std::string config::getDefaultConfig() {
-	// Temporarily using gists, when source is public use: https://github.com/viown/Atomic/blob/master/Data/DefaultConfig.cfg
-	cpr::Response defaultConfig = cpr::Get(cpr::Url{ "https://gist.githubusercontent.com/viown/3c59e04dc82cdf07fce52e0b4a10b020/raw/4366851e0f07a6664f3ae3b4a52b93bc9e5b5744/config.cfg" });
+	cpr::Response defaultConfig = cpr::Get(cpr::Url{ "https://raw.githubusercontent.com/viown/Atomic/master/Data/DefaultConfig.cfg" });
 	if (!atomic::isStatusSuccess(defaultConfig.status_code))
 		throw atomic::HttpError{ "ConfigFetchFailure", defaultConfig.status_code };
 	return defaultConfig.text;
