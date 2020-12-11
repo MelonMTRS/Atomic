766eda9a68d9f13ade8c31beef1febfee2785154
viown <48097677+viown@users.noreply.github.com>
2020-12-11 09:13:58 +0000

    Using std::reduce

--- Atomic/Atomic/Offer.h
+++ Atomic/Atomic/Offer.h
@@ -28,28 +28,24 @@ namespace atomic {
 		[[nodiscard]] inline const int& getRobuxRequested() const noexcept { return this->m_robuxRequesting; }
 
 		[[nodiscard]] inline const std::int64_t getTotalOfferedRap() const {
-			std::int64_t sum = 0;
-			for (auto offer = this->m_offering.begin(); offer != this->m_offering.end(); ++offer)
-				sum += offer->rap;
-			return sum;
+			return std::reduce(this->m_offering.begin(), this->m_offering.end(), std::int64_t{0}, [](std::int64_t accumulator, atomic::UniqueItem item) -> std::int64_t {
+				return accumulator + item.rap;
+			});
 		}
 		[[nodiscard]] inline const std::int64_t getTotalRequestedRap() const {
-			std::int64_t sum = 0;
-			for (auto request = this->m_requesting.begin(); request != this->m_requesting.end(); ++request)
-				sum += request->rap;
-			return sum;
+			return std::reduce(this->m_requesting.begin(), this->m_requesting.end(), std::int64_t{ 0 }, [](std::int64_t accumulator, atomic::UniqueItem item) -> std::int64_t {
+				return accumulator + item.rap;
+			});
 		}
 		[[nodiscard]] inline const std::int64_t getTotalOfferedValue() const {
-			std::int64_t sum = 0;
-			for (auto offer = this->m_offering.begin(); offer != this->m_offering.end(); ++offer)
-				sum += offer->value;
-			return sum;
+			return std::reduce(this->m_offering.begin(), this->m_offering.end(), std::int64_t{ 0 }, [](std::int64_t accumulator, atomic::UniqueItem item) -> std::int64_t {
+				return accumulator + item.value;
+			});
 		}
 		[[nodiscard]] inline const std::int64_t getTotalRequestedValue() const {
-			std::int64_t sum = 0;
-			for (auto request = this->m_requesting.begin(); request != this->m_requesting.end(); ++request)
-				sum += request->value;
-			return sum;
+			return std::reduce(this->m_requesting.begin(), this->m_requesting.end(), std::int64_t{ 0 }, [](std::int64_t accumulator, atomic::UniqueItem item) -> std::int64_t {
+				return accumulator + item.value;
+			});
 		}
 	};
 }
